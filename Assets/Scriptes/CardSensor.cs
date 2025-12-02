5322fbcc889084b773749a3793915cbeb18cc075
Phyz <nathan.vigon@etu.univ-amu.fr>
2025-12-02 19:13:18 +0100

    tp marche pas

--- Assets/Scriptes/CardSensor.cs
+++ Assets/Scriptes/CardSensor.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 public class CardSensor : MonoBehaviour
 {
     public int Value;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<CardInteraction>(out CardInteraction card))
+        {
+            card.InteractWithSensor(Value);
+        }
+    }
 }
