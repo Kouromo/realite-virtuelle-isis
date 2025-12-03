bc22c12fa64e66d42df5ac9b14af8378f13a5179
Matéo Alves <blackwolf4728@gmail.com>
2025-12-03 14:58:24 +0100

    Can't be spammable

--- Assets/Scripts/RollingSlot.cs
+++ Assets/Scripts/RollingSlot.cs
@@ -19,7 +19,10 @@ public class RollingSlot : MonoBehaviour
     public float durationRotationMax = 3f;
 
     private float durationStop = 1;
-    private float durationStopCurrent = 0;
+
+    // guard to avoid starting multiple spin sessions
+    private bool isSpinning = false;
+    private int remainingRollers = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -35,12 +38,34 @@ public class RollingSlot : MonoBehaviour
 
     public void PullLever()
     {
+        if (isSpinning)
+            return;
+
+        if (rollers == null || rollers.Length == 0)
+            return;
+
+        // mark spinning and count valid rollers
+        isSpinning = true;
+        remainingRollers = 0;
+        for (int i = 0; i < rollers.Length; i++)
+            if (rollers[i] != null)
+                remainingRollers++;
+
+        if (remainingRollers == 0)
+        {
+            isSpinning = false;
+            return;
+        }
+
         for (int i = 0; i < rollers.Length; i++)
         {
+            if (rollers[i] == null)
+                continue;
+
             int notchRandomFinal = Random.Range(0, NUM_SYMBOLS);
 
             float angleCible = notchRandomFinal * angleCran + angleCranCalibrate;
-            float MoreSpin = 360 * NUM_SYMBOLS;
+            float MoreSpin = 360f * NUM_SYMBOLS;
 
             StartCoroutine(RotateAndStop(rollers[i], angleCible + MoreSpin, duration));
         }
@@ -81,6 +106,12 @@ public class RollingSlot : MonoBehaviour
         Vector3 rotationFinalePrecise = new(rotationTotaleCible % 360f, rotationFinale.y, rotationFinale.z);
         rouleau.transform.localEulerAngles = rotationFinalePrecise;
 
-        // TODO: Notifier le script principal que ce rouleau est arrêté
+        // mark this roller finished and clear spinning flag when all done
+        remainingRollers--;
+        if (remainingRollers <= 0)
+        {
+            isSpinning = false;
+            Debug.Log("All rollers stopped");
+        }
     }
 }
