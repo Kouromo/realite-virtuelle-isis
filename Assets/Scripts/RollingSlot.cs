86c9021430802e19554a9ae805e3d8ccb0d38002
Matéo Alves <blackwolf4728@gmail.com>
2025-12-03 12:34:57 +0100

    Teleportation manager

--- Assets/Scripts/RollingSlot.cs
+++ Assets/Scripts/RollingSlot.cs
@@ -8,6 +8,7 @@ public class RollingSlot : MonoBehaviour
 
     private float angleCranCalibrate = 50;
 
+    
 
     public GameObject[] rollers;
 
@@ -19,7 +20,6 @@ public class RollingSlot : MonoBehaviour
     public float durationRotationMax = 3f;
 
     private float durationStop = 1;
-    private float durationStopCurrent = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
